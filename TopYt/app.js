aa7fc22f1ea74580ec0ec2100d2f42b523338430
v786 <jahangirali786@gmail.com>
2019-03-10 20:41:29 +0530

    Added react app, added UI elemetns, need to take prop

--- TopYt/app.js
+++ TopYt/app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
+var cors = require('cors')
 
 var router = require('./routes/routes');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/', router);
 app.use('/users', usersRouter);
